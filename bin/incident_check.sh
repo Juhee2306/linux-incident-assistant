a539501df31c2508ae29b8c787c9958b8d5b346e
Juhee Lavanya <juheelavanya@gmail.com>
2026-02-07 11:36:48 +0000

    Add memory pressure incident detection

--- bin/incident_check.sh
+++ bin/incident_check.sh
@@ -17,5 +17,19 @@ then
 	echo "WARNING:Disk usage is ${DISK_USAGE}% >= ${DISK_THRESHOLD}%" >> "$LOG_FILE"
 	echo "Impact: Logs/services may fail due to disk pressure" >>  "$LOG_FILE"
 else
-	echo " OK: Disk usage is ${DISK_USAGE}%" >> "$LOG_FILE"
+	echo "OK: Disk usage is ${DISK_USAGE}%" >> "$LOG_FILE"
 fi
+
+MEM_THRESHOLD=80
+MEMORY_TOTAL=$(free | awk '/Mem:/ {print $2}')
+MEMORY_USED=$(free| awk '/Mem:/ {print $3}')
+MEM_PER=$(( MEMORY_USED * 100 /MEMORY_TOTAL ))
+
+if [[ "$MEM_PER" -ge "$MEM_THRESHOLD" ]]
+then
+	echo "WARNING: Memory usage is ${MEM_PER}% >= ${MEM_THRESHOLD}%" >> $LOG_FILE
+	echo "Impact: Processes may slow down or get killed due to memory pressure" >> $LOG_FILE
+else
+	echo "OK: Memory usage is ${MEM_PER}%" >> $LOG_FILE
+fi
+
