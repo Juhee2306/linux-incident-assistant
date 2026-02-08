2e168af61376edfdd698827ee996b1ca97f94ce8
Juhee Lavanya <juheelavanya@gmail.com>
2026-02-08 09:44:21 +0000

    Refactor incident checks into functions

--- bin/incident_check.sh
+++ bin/incident_check.sh
@@ -5,41 +5,57 @@ LOG_FILE="$LOG_DIR/incident.log"
 
 mkdir -p "$LOG_DIR"
 
-echo "============================" >> "$LOG_FILE"
-echo "Incident check ran at: $(date)" >> "$LOG_FILE"
+log(){
+	echo "$1" >> "$LOG_FILE"
+}
 
-DISK_THRESHOLD=80
+disk_check(){
+	DISK_THRESHOLD=80
+	DISK_USAGE=$(df / | tail -1 | awk '{print $5}' | tr -d '%')
+	if [[ "$DISK_USAGE" -ge "$DISK_THRESHOLD" ]]
+	then
+		log "WARNING:Disk usage is ${DISK_USAGE}% >= ${DISK_THRESHOLD}%"
+		log "Impact: Logs/services may fail due to disk pressure"
+	else
+		log "OK: Disk usage is ${DISK_USAGE}%" 
+	fi
 
-DISK_USAGE=$(df / | tail -1 | awk '{print $5}' | tr -d '%')
+}
 
-if [[ "$DISK_USAGE" -ge "$DISK_THRESHOLD" ]]
-then
-	echo "WARNING:Disk usage is ${DISK_USAGE}% >= ${DISK_THRESHOLD}%" >> "$LOG_FILE"
-	echo "Impact: Logs/services may fail due to disk pressure" >>  "$LOG_FILE"
-else
-	echo "OK: Disk usage is ${DISK_USAGE}%" >> "$LOG_FILE"
-fi
+memory_check(){
+	MEM_THRESHOLD=80
+	MEMORY_TOTAL=$(free | awk '/Mem:/ {print $2}')
+	MEMORY_USED=$(free| awk '/Mem:/ {print $3}')
+	MEM_PER=$(( MEMORY_USED * 100 /MEMORY_TOTAL ))
+	if [[ "$MEM_PER" -ge "$MEM_THRESHOLD" ]]
+	then
+		log "WARNING: Memory usage is ${MEM_PER}% >= ${MEM_THRESHOLD}%" 
+		log "Impact: Processes may slow down or get killed due to memory pressure" 
+	else
+		log "OK: Memory usage is ${MEM_PER}%"
+	fi
 
-MEM_THRESHOLD=80
-MEMORY_TOTAL=$(free | awk '/Mem:/ {print $2}')
-MEMORY_USED=$(free| awk '/Mem:/ {print $3}')
-MEM_PER=$(( MEMORY_USED * 100 /MEMORY_TOTAL ))
+}
 
-if [[ "$MEM_PER" -ge "$MEM_THRESHOLD" ]]
-then
-	echo "WARNING: Memory usage is ${MEM_PER}% >= ${MEM_THRESHOLD}%" >> $LOG_FILE
-	echo "Impact: Processes may slow down or get killed due to memory pressure" >> $LOG_FILE
-else
-	echo "OK: Memory usage is ${MEM_PER}%" >> $LOG_FILE
-fi
+ssh_check(){
+	if pgrep -x sshd > /dev/null
+	then
+		log "OK: SSH daemon is running" 
+	else
+		log "WARNING: SSH daemon is NOT running"
+		log "Impact: Remote access to this system is unavailable" 
+	fi
+
+}
+
+main(){
+	log "============================"
+        log "Incident check ran at: $(date)"
 
 
+	disk_check
+	memory_check
+	ssh_check
 
-if pgrep -x sshd > /dev/null
-then
-	echo "OK: SSH daemon is running" >> "$LOG_FILE"
-else
-	echo "WARNING: SSH daemon is NOT running" >> "$LOG_FILE"
-	echo "Impact: Remote access to this system is unavailable" >> "$LOG_FILE"
-fi
-
+}
+main
